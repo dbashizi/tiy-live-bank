770c61b1afe9b07b41c506e3edb4acaed942c5dc
Jacquetta <jacquettamorton@gmail.com>
2016-12-08 10:55:30 -0500

    Added Jacquetta Implementation

---
+++ com/tiy/practice/JacquettaMorton.java
@@ -0,0 +1,15 @@
+package com.tiy.practice;
+
+/**
+ * Created by jacquettamorton on 12/8/16.
+ */
+public class JacquettaMorton {
+
+    public JacquettaMorton() {
+
+    }
+    public void printInfo(){
+        System.out.println("I'm Jacquetta");
+
+    }
+}
---
+++ com/tiy/practice/JacquettaRunner.java
@@ -0,0 +1,13 @@
+package com.tiy.practice;
+
+/**
+ * Created by jacquettamorton on 12/8/16.
+ */
+public class JacquettaRunner {
+
+public static void main(String[] args){
+
+    JacquettaMorton jackie = new JacquettaMorton();
+            jackie.printInfo();
+}
+}
