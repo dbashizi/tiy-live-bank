ca8f03193667bea5c622e68b676bd674855b1595
Kwasi D <Kwasi D>
2016-12-08 11:09:15 -0500

    Added Kwasi implementation

---
+++ com/tiy/practice/Kwasi.java
@@ -0,0 +1,10 @@
+package com.tiy.practice;
+
+/**
+ * Created by Kwasi on 12/8/2016.
+ */
+public class Kwasi {
+    public void printInfo(){
+        System.out.println("Kwaz was here!");
+    }
+}
