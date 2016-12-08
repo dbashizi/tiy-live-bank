c4b8357c512369e3b5496167e37eab62d1ce4288
Dominique Bashizi <dominique@blupods.com>
2016-12-08 10:55:21 -0500

    Added Dominique Implementation

---
+++ com/tiy/practice/Dominique.java
@@ -0,0 +1,10 @@
+package com.tiy.practice;
+
+/**
+ * Created by dbashizi on 12/8/16.
+ */
+public class Dominique {
+    public void printInfo() {
+        System.out.println("I'm Dominique");
+    }
+}
---
+++ com/tiy/practice/DominiqueRunner.java
@@ -0,0 +1,10 @@
+package com.tiy.practice;
+
+/**
+ * Created by dbashizi on 12/8/16.
+ */
+public class DominiqueRunner {
+    public static void main(String[] args) {
+        new Dominique().printInfo();
+    }
+}
