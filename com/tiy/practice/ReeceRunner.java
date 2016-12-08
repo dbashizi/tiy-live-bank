1a878d002176d0841c00fa6aa3b74076e15d9f77
Maurice Thomas <mauricet1520@gmail.com>
2016-12-08 10:55:32 -0500

    changes made

---
+++ com/tiy/practice/ReeceRunner.java
@@ -0,0 +1,15 @@
+package com.tiy.practice;
+
+/**
+ * Created by crci1 on 12/8/2016.
+ */
+public class ReeceRunner {
+
+    public static void main(String[] args){
+        Reece cr = new Reece();
+        cr.printInfo();
+
+
+    }
+
+}
