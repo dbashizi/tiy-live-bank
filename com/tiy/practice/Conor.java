983e8ff32675d2afbc2a088baadbb33ecf2ea27a
Conor Thomas <conorthomas@Conors-Air.lan>
2016-12-08 10:58:58 -0500

    added Conor implementation

---
+++ com/tiy/practice/Conor.java
@@ -0,0 +1,10 @@
+package com.tiy.practice;
+
+/**
+ * Created by conorthomas on 12/8/16.
+ */
+public class Conor {
+    public void printInfo() {
+        System.out.println("Hello my name is Conor");
+    }
+}
