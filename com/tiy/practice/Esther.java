755db7af76e13cf869b034753bde0fbb903a3c8e
Esther Osasogie <eosasogie@MacBook-Pro.lan>
2016-12-08 10:55:28 -0500

    Added Esther Implementation

---
+++ com/tiy/practice/Esther.java
@@ -0,0 +1,11 @@
+package com.tiy.practice;
+
+/**
+ * Created by eosasogie on 12/8/16.
+ */
+public class Esther {
+    public void printInfo() {
+        System.out.println("Hey Esther");
+
+    }
+}
